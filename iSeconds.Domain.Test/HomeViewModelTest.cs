537f001dfa62270a75c6e366fdffbdf8cd7a6598
martim <martim@0369b9bc-993e-4c8f-8a90-32d0e374cfa0>
2013-03-14 03:20:06 +0000

    Subindo testes
    git-svn-id: https://xp-dev.com/svn/iseconds/trunk@32 0369b9bc-993e-4c8f-8a90-32d0e374cfa0

---
+++ iSeconds.Domain.Test/HomeViewModelTest.cs
@@ -0,0 +1,56 @@
+using NUnit.Framework;
+using System;
+using iSeconds.Domain;
+using System.ComponentModel;
+
+namespace iSeconds.Domain.Test
+{
+	[TestFixture()]
+	public class HomeViewModelTest
+	{
+		ISecondsDB repository = null; 
+		User user = null;
+
+		[SetUp()]
+		public void Init()
+		{
+			repository = new ISecondsDB("testbase.db3");
+			repository.DeleteAll<User> ();
+			user = new User ("xuxa");
+			repository.SaveItem (user);
+		}
+
+		class MockHomeView
+		{
+		}
+
+		[Test()]
+		public void TestUserWithoutTimelineHasNoCurrentTimeline ()
+		{
+			HomeViewModel viewModel = new HomeViewModel (user, repository);
+            Assert.Null(viewModel.CurrentTimeline);
+            Assert.IsEmpty(viewModel.Timelines);
+		}
+
+        [Test()]
+        public void TestModelViewNotifiesOnNewTimeline()
+        {
+            HomeViewModel viewModel = new HomeViewModel(user, repository);
+            MockHomeView view = new MockHomeView();
+
+            bool wasCalled = false;
+            viewModel.PropertyChanged += (object sender, PropertyChangedEventArgs args) =>
+            {
+                Assert.AreEqual("CurrentTimeline", args.PropertyName);
+                wasCalled = true;
+
+            };
+
+            repository.SaveTimeline(new Timeline("xou da xuxa", user.Id));
+
+            Assert.IsTrue(wasCalled);
+        }
+
+	}
+}
+
---
+++ iSeconds.Domain.Test/TimelineViewModelTest.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace iSeconds.Domain.Test
+{
+    [TestFixture()]
+    class TimelineViewModelTest
+    {
+        User user = null;
+        Timeline timeline = null;
+        ISecondsDB repository = null;        
+
+        [SetUp()]
+        public void Init()
+        {
+            repository = new ISecondsDB("testbase.db3");
+            repository.DeleteAll<User>();
+            user = new User("xuxa");
+            repository.SaveItem(user);
+            timeline = new Timeline("xou da xuxa", user.Id);
+            repository.SaveTimeline(timeline);
+        }
+
+        [Test()]
+        public void TestEmptyTimelineShouldHaveNoDays()
+        {
+            TimelineViewModel viewModel = new TimelineViewModel(timeline, repository);
+            Assert.IsEmpty(viewModel.Days);
+        }
+
+        [Test()]
+        public void TestOnCurrentDateChangedVisibleDaysChangesToo()
+        {
+            TimelineViewModel viewModel = new TimelineViewModel(timeline, repository);
+
+            viewModel.CalendarMode = TimelineViewModel.VisualizationMode.MONTH;            
+            viewModel.CurrentDate = new DateTime(2013, 3, 12);
+
+            Assert.That(viewModel.VisibleDays.Count, Is.EqualTo(42)); // fixed...
+            Assert.That(viewModel.VisibleDays[0].date, Is.EqualTo(new DateTime(2013, 2, 24)));
+            Assert.That(viewModel.VisibleDays[viewModel.VisibleDays.Count-1].date, Is.EqualTo(new DateTime(2013, 4, 6)));
+        }
+
+        [Test()]
+        public void TestOnNewDay()
+        {
+            //TimelineViewModel viewModel = new TimelineViewModel(timeline, repository);
+
+            //viewModel.AddVideoAt.Execute(DateTime.Today, "video.path");
+        }
+    }
+}
