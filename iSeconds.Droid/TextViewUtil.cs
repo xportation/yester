7f35f9f38c9965313ff4758843ed480395120ce0
xportation <xportation@0369b9bc-993e-4c8f-8a90-32d0e374cfa0>
2013-05-01 17:40:12 +0000

    #41 #42 Arquivo que faltou no commit
    git-svn-id: https://xp-dev.com/svn/iseconds/trunk@64 0369b9bc-993e-4c8f-8a90-32d0e374cfa0

---
+++ iSeconds.Droid/TextViewUtil.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Views;
+using Android.Widget;
+using LegacyBar.Library.Bar;
+using iSeconds.Domain;
+using System.ComponentModel;
+using Android.Graphics;
+
+namespace iSeconds.Droid
+{
+   internal class TextViewUtil	
+	{
+		static private void ChangeFont(TextView textView, Context context, string fontName, float fontSize)
+		{
+			Typeface tf = Typeface.CreateFromAsset(context.Assets, fontName);
+			textView.SetTypeface(tf,TypefaceStyle.Normal);
+			textView.TextSize= fontSize;
+		}
+
+		static public void ChangeFontForActionBarTitle(TextView textView, Context context, float fontSize)
+		{
+			ChangeFont(textView,context,"fonts/augie.ttf",fontSize);
+		}
+				
+		static public void ChangeFontForMonthTitle(TextView textView, Context context, float fontSize)
+		{
+			ChangeFont(textView,context,"fonts/Andy Bold.ttf",fontSize);
+		}
+
+		public static void ChangeFontForTimelinesList (TextView textView, Context context, float fontSize)
+		{
+			ChangeFont(textView,context,"fonts/Andy Bold.ttf",fontSize);
+		}
+	}
+   
+}
