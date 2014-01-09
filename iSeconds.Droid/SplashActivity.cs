1e4fefb2bd77fb8b36c9629479f55236e192cf10
xportation <xportation@0369b9bc-993e-4c8f-8a90-32d0e374cfa0>
2014-01-09 00:50:21 +0000

    Preparacao para bloquear usuarios betas que nao forem cadastrados... :p
    git-svn-id: https://xp-dev.com/svn/iseconds/trunk@155 0369b9bc-993e-4c8f-8a90-32d0e374cfa0

--- iSeconds.Droid/SplashActivity.cs
+++ iSeconds.Droid/SplashActivity.cs
@@ -5,6 +5,8 @@ using iSeconds.Domain;
 using Android.Views;
 using Android.Widget;
 using iSeconds.Domain.Framework;
+using Android.Telephony;
+using Android.Accounts;
 
 namespace iSeconds.Droid
 {
@@ -15,10 +17,49 @@ namespace iSeconds.Droid
 		{
 			base.OnCreate (bundle);
          try {
-				navigator.NavigateTo("timeline_view", new Args());
+				if (isABetaTester())
+					navigator.NavigateTo("timeline_view", new Args());
+				else
+					betaTesterRestricionMessage();
          } catch (Exception) {
             this.Finish();
          }
+		}
+
+		private bool isABetaTester()
+		{
+			AccountManager am = AccountManager.Get(this);
+			Account[] accounts = am.GetAccounts();
+
+			foreach (Account ac in accounts) {
+				if (isAccountAllowed(ac.Name)) {
+					return true;
+				}
+			}
+			return false;
+		}
+
+		private void betaTesterRestricionMessage()
+		{
+			new AlertDialog.Builder(this)
+				.SetTitle (string.Empty)
+				.SetMessage ("Esta versão é apenas para usuários beta cadastrados.\nSe você quiser testá-la também, por favor peça para quem lhe passou indicar você.\n\nInfelizmente essa restrição é necessária para evitar usuários manjadores.")
+				.SetPositiveButton(Resource.String.ok, delegate { this.Finish(); })
+				.Show ();
+		}
+
+		private bool isAccountAllowed(string name)
+		{
+			if (name.Contains("xportation@gmail.com"))
+				return true;
+			else if (name.Contains("martim00@gmail.com"))
+				return true;
+			else if (name.Contains("biaosiegel@gmail.com"))
+				return true;
+			else if (name.Contains("ronald.paloschi@gmail.com"))
+				return true;
+
+			return false;
 		}
 	}
 }
