cef5ecef1c781a6977d56849f97a7c5777397f09
ronald.paloschi <ronald.paloschi@0369b9bc-993e-4c8f-8a90-32d0e374cfa0>
2014-03-21 20:56:05 +0000

    * Fixed a bug that happens often on android 4.4 where the OS records the video normally (0 or 90 degrees), but sets the "real" rotation (0, 90, 180, 270) as a metadata on the media file. The real rotation depends on the position of the camera. Now when generating a compilation, the video is transposed as needed and the metadata is set to rotate 0 degrees. The final outcome is only possible to be 0 or 90 degrees rotation.  * Fixed a bunch of small bugs.  * Changed tabs to spaces on some files.
    git-svn-id: https://xp-dev.com/svn/iseconds/trunk@219 0369b9bc-993e-4c8f-8a90-32d0e374cfa0

--- iSeconds.Droid/ffmpeg_wrapper/VideoFileInformation.cs
+++ iSeconds.Droid/ffmpeg_wrapper/VideoFileInformation.cs
@@ -3,57 +3,58 @@ using System.Text.RegularExpressions;
 
 namespace iSeconds.Droid
 {
-	public class VideoFileInformation
-	{
-		private string _Path;
-		public string Path
-		{
-			get
-			{
-				return _Path;
-			}
-			set
-			{
-				_Path = value;
-			}
-		}
+    public class VideoFileInformation
+    {
+        private string _Path;
+        public string Path
+        {
+            get
+            {
+                return _Path;
+            }
+            set
+            {
+                _Path = value;
+            }
+        }
 
-		public TimeSpan Duration { get; set; }
-		public double BitRate { get; set; }
-		public string AudioFormat { get; set; }
-		public string VideoFormat { get; set; }
-		public int Height { get; set; }
-		public int Width { get; set; }
-		public double Fps { get; set; }
+        public TimeSpan Duration { get; set; }
+        public double BitRate { get; set; }
+        public string AudioFormat { get; set; }
+        public string VideoFormat { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public double Fps { get; set; }
+        public int Rotation { get; set; }
 
-		public int AudioFrequencyInHz()
-		{
-			var re = new Regex("(\\d{1,7}) Hz,");
-			var m = re.Match(this.AudioFormat);
+        public int AudioFrequencyInHz()
+        {
+            var re = new Regex("(\\d{1,7}) Hz,");
+            var m = re.Match(this.AudioFormat);
 
-			int frequency = 41000;
-			if (m.Success)
-				int.TryParse(m.Groups[1].Value, out frequency);
+            int frequency = 41000;
+            if (m.Success)
+                int.TryParse(m.Groups[1].Value, out frequency);
 
-			return frequency;
-		}
+            return frequency;
+        }
 
-		public double AudioBitRateInKBits()
-		{
-			var lastPart = this.AudioFormat.Substring (this.AudioFormat.LastIndexOf (',') + 1).Trim();
+        public double AudioBitRateInKBits()
+        {
+            var lastPart = this.AudioFormat.Substring(this.AudioFormat.LastIndexOf(',') + 1).Trim();
 
-			string[] parts = lastPart.Split(' ');
+            string[] parts = lastPart.Split(' ');
 
-			double bitrate = 128.0;
-			if (parts.Length >= 2) 
-				double.TryParse (parts [0], out bitrate);
+            double bitrate = 128.0;
+            if (parts.Length >= 2)
+                double.TryParse(parts[0], out bitrate);
 
-			return bitrate;
-		}
+            return bitrate;
+        }
 
-		public VideoFileInformation()
-		{
-		}
-	}
+        public VideoFileInformation()
+        {
+        }
+    }
 }
 
