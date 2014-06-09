dfff2ea4ef978ba3506e27bcc858abbe603283c3
Leonardo <xportation@gmail.com>
2014-06-08 23:49:09 -0300

    Ajustei o teste do SqlUtils para funcionar

--- jYester/test/SqlUtilsTest.java
+++ jYester/test/SqlUtilsTest.java
@@ -1,35 +1,49 @@
+import static org.junit.Assert.assertEquals;
+import iSeconds.Domain.SqlUtils;
+
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
-import iSeconds.Domain.SqlUtils;
+import junit.framework.Assert;
 
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public class SqlUtilsTest {
 	
-	private boolean assertDate(Date date, int year, int month, int day) {
+	private void assertDate(Date date, int year, int month, int day) {
+		org.junit.Assert.assertNotNull(date);
+		
 		Calendar calendar = new GregorianCalendar();
 		calendar.setTime(date);
 		assertEquals(year, calendar.get(Calendar.YEAR));
-		assertEquals(month, calendar.get(Calendar.MONTH));
+		assertEquals(month-1, calendar.get(Calendar.MONTH));
 		assertEquals(day, calendar.get(Calendar.DAY_OF_MONTH));
+	}
+	
+	private Date getDate(String dateStr) {
+		try {
+			return SqlUtils.parseDate(dateStr);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 	
 	@Test
 	public void parseDate() 
 	{
-		Date date= SqlUtils.parseDate("2013-06-05 00:00 PM");
+		Date date= getDate("2013-06-05 00:00 PM");
 		assertDate(date, 2013, 6, 5);
 		
-		date= SqlUtils.parseDate("2013-12-30 00:00 PM");
+		date= getDate("2013-12-30 00:00 PM");
 		assertDate(date, 2013, 12, 30);
 		
-		date= SqlUtils.parseDate("2014-07-31 00:00 PM");
+		date= getDate("2014-07-31 00:00 PM");
 		assertDate(date, 2014, 7, 31);
 	}
 }
